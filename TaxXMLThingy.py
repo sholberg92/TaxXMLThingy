01b695605bf05df48192db5aed6203cb02ddd690
sholberg92 <sholberg92@gmail.com>
2016-04-20 20:24:59 -0500

    Fix bugs with csv formatting

--- TaxXMLThingy.py
+++ TaxXMLThingy.py
@@ -26,22 +26,26 @@ fnInsertPos = outputPath.find('.xml')
 rows = []
 
 for row in csvTaxDocument:
-    rows.append(row)
-
+    if row[0] != '':
+        rows.append(row)
+    
+print(rows)
 numberOfRows = 14 # set to how many rows in the table in the destination document
 
+print(len(rows))
 
 for x in range(0, len(rows), numberOfRows):
-    i = x        
-    for xmlRow in root.find('Page1').find('Table_Line1'):
-        
-        j = 0
+    i = x # preserve x index and use i for inner loop
+    for xmlRow in root.find('Page1').find('Table_Line1'):       
+        j = 0 # index for iterating over rows in XML doc             
         for xmlCol in xmlRow:
-            xmlCol.text = str(rows[i][j])
-            j += 1
-            
-        i += 1
-    tree.write(outputPath[:fnInsertPos] + str(x//numberOfRows + 1) + outputPath[fnInsertPos:]);
+            if i < len(rows): # update xml row with values if the value exists in rows array
+                xmlCol.text = str(rows[i][j])
+                j += 1 # next xml row
+            else: # if no more data in rows fill remaining xml rows with blank data
+                xmlCol.text = ''          
+        i += 1 # next row in xml doc
+    tree.write(outputPath[:fnInsertPos] + str(x//numberOfRows + 1) + outputPath[fnInsertPos:]); # write data to new xml file
 
 print("Done!")
 taxDocument.close()
